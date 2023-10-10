2676ca6d5e1695038498f421be62a76644956fd9
Natte <107698186+Natteens@users.noreply.github.com>
2023-10-10 19:46:28 -0300

    cena alterada

--- Spyro Eternal Night Remake/Assets/Resources/Scripts/GameManager/FallMap.cs
+++ Spyro Eternal Night Remake/Assets/Resources/Scripts/GameManager/FallMap.cs
@@ -1,27 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.SceneManagement;
 
 public class FallMap : MonoBehaviour
 {
-    public Transform player; // Referência ao jogador
-    public CinemachineFreeLook freeLookCamera; // Referência à FreeLook Camera do Cinemachine
-    public LayerMask fallLayer; // A camada que representa a queda no Inspector
-    public float restartDelay = 2.0f; // Tempo de atraso antes de reiniciar a cena
+    public Transform player; 
+    public CinemachineFreeLook freeLookCamera; 
+    public LayerMask fallLayer; 
+    public float restartDelay = 2.0f; 
 
     private bool isFalling = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Verifique se o jogador entrou em um collider da camada de queda
         if ((fallLayer.value & 1 << other.gameObject.layer) != 0)
         {
-            // Pare de seguir o jogador e desabilite o controle do mouse na câmera
             freeLookCamera.m_Follow = null;
             freeLookCamera.m_XAxis.m_InputAxisName = "";
             freeLookCamera.m_YAxis.m_InputAxisName = "";
-
-            // Inicie o processo de reinício da cena após um atraso
             if (!isFalling)
             {
                 isFalling = true;
@@ -32,7 +30,6 @@ public class FallMap : MonoBehaviour
 
     private void RestartScene()
     {
-        // Reinicie a cena do jogo após o atraso especificado
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Spyro Eternal Night Remake/Assets/Resources/Scripts/World/Plataformas/PlataformaCircular.cs
+++ Spyro Eternal Night Remake/Assets/Resources/Scripts/World/Plataformas/PlataformaCircular.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlataformaCircular : MonoBehaviour
@@ -28,7 +30,6 @@ public class PlataformaCircular : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Desconecta o jogador do pai anterior antes de conectá-lo à plataforma
             other.transform.parent = null;
         }
     }
@@ -37,16 +38,11 @@ public class PlataformaCircular : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Mantém o jogador na mesma altura que a plataforma
             Vector3 newPosition = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
             other.transform.position = newPosition;
-
-            // Calcula a rotação relativa da plataforma e aplica ao jogador
             Vector3 relativePosition = other.transform.position - transform.position;
             float angle = Mathf.Atan2(relativePosition.x, relativePosition.z) * Mathf.Rad2Deg;
             other.transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            // Conecta o jogador à plataforma
             other.transform.parent = transform;
         }
     }
@@ -55,7 +51,6 @@ public class PlataformaCircular : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Desconecta o jogador da plataforma ao sair do trigger
             other.transform.parent = null;
         }
     }
