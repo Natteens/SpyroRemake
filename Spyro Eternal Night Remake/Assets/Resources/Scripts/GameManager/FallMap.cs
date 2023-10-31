a59f814f57ca6982fa96250eb26f94703bb65db6
Natte <107698186+Natteens@users.noreply.github.com>
2023-10-31 18:22:15 -0300

    build 31/10

--- Spyro Eternal Night Remake/Assets/Resources/Scripts/GameManager/FallMap.cs
+++ Spyro Eternal Night Remake/Assets/Resources/Scripts/GameManager/FallMap.cs
@@ -4,35 +4,52 @@ using UnityEngine.SceneManagement;
 
 public class FallMap : MonoBehaviour
 {
-    public Transform player; // Referência ao jogador
-    public CinemachineFreeLook freeLookCamera; // Referência à FreeLook Camera do Cinemachine
-    public LayerMask fallLayer; // A camada que representa a queda no Inspector
-    public float restartDelay = 2.0f; // Tempo de atraso antes de reiniciar a cena
+    public Transform player;
+    public CinemachineFreeLook freeLookCamera;
+    public LayerMask fallLayer;
+    public float restartDelay = 2.0f;
+    public float fallTimeThreshold = 3.0f; // Limiar de tempo para considerar uma queda
+    public float fallHeightThreshold = -25.0f; // Altura para considerar uma queda
 
     private bool isFalling = false;
+    private float fallTimer = 0f;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Verifique se o jogador entrou em um collider da camada de queda
         if ((fallLayer.value & 1 << other.gameObject.layer) != 0)
         {
-            // Pare de seguir o jogador e desabilite o controle do mouse na câmera
             freeLookCamera.m_Follow = null;
             freeLookCamera.m_XAxis.m_InputAxisName = "";
             freeLookCamera.m_YAxis.m_InputAxisName = "";
-
-            // Inicie o processo de reinício da cena após um atraso
             if (!isFalling)
             {
                 isFalling = true;
-                Invoke("RestartScene", restartDelay);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isFalling)
+        {
+            fallTimer += Time.deltaTime;
+
+            // Adicione a lógica aqui para verificar se o jogador está caindo por mais de alguns segundos
+            if (fallTimer >= fallTimeThreshold)
+            {
+                RestartScene();
+            }
+
+            // Verificar a altura do jogador
+            if (player.position.y <= fallHeightThreshold)
+            {
+                RestartScene();
             }
         }
     }
 
     private void RestartScene()
     {
-        // Reinicie a cena do jogo após o atraso especificado
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
